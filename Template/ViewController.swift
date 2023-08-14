980200598d1bbc41db63ee46ad3590f173427bd6
kimhokyun <cjswoghrbs@gmail.com>
2023-08-15 00:07:05 +0900

    2nd

--- Template/ViewController.swift
+++ Template/ViewController.swift
@@ -22,11 +22,11 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.dataSource = self
+//        super.viewDidLoad()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         let decoder: JSONDecoder = JSONDecoder()
         if let data = UserDefaults.standard.object(forKey: "SampleData") as? Data,
@@ -40,32 +40,47 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController:UITableViewDelegate,UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sampleDatas.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as! CustomTableViewCell
-        let celldata = self.sampleDatas[indexPath.row]
-        
-//        cell.titleLabel.text = celldata.title
-//        cell.contentsLabel.text = celldata.contents
-//        guard let pd = celldata.photo else {return cell}
-//        cell.photoImageView.image =  UIImage(data: pd)
-        
-        return cell
-    }
-}
+//extension ViewController:UITableViewDelegate,UITableViewDataSource{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return sampleDatas.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as! CustomTableViewCell
+//        let celldata = self.sampleDatas[indexPath.row]
+//
+////        cell.titleLabel.text = celldata.title
+////        cell.contentsLabel.text = celldata.contents
+////        guard let pd = celldata.photo else {return cell}
+////        cell.photoImageView.image =  UIImage(data: pd)
+//
+//        return cell
+//    }
+//}
 
-extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sampleDatas.count
+        if section == 0{
+            return 7
+        }else{
+            //            return sampleDatas.count
+            return 42
+        }
+        
+
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
-        let celldata = self.sampleDatas[indexPath.row]
+//        let celldata = self.sampleDatas[indexPath.row]
+        
+        if indexPath.section == 0 {
+//            cell.dayLabel.text = "요일"
+        }else {
+            
+        }
+        
         
 //        cell.titleLabel.text = celldata.title
 //        cell.contentsLabel.text = celldata.contents
@@ -75,6 +90,25 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource{
         return cell
     }
     
+    // 셀 크기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+      
+        let numberOfColumns: CGFloat = 7  // Number of columns in a week
+        let cellWidth = collectionView.bounds.width / numberOfColumns
+        let cellSize = CGSize(width: cellWidth, height: cellWidth)
+        
+        return cellSize
+    }
+    
+    // 그리드의 항목 줄 사이에 사용할 최소 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    // 같은 행에 있는 항목 사이에 사용할 최소 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
 
 extension ViewController:SendDataProtocol{
